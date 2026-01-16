09e6db1fa6aa4c45c707c5d28c579a0c1e3cef84
Mateus <164413647+mattsousas@users.noreply.github.com>
2026-01-16 11:45:00 -0300

    Delete energia-main directory

--- energia-main/src/scroll.js
+++
@@ -1,6 +0,0 @@
-function scrollToCalculator() {
-    const calculatorSection = document.getElementById('calculadora');
-    if (calculatorSection) {
-        calculatorSection.scrollIntoView({ behavior: 'smooth' });
-    }
-}
